cdde4d3620b3ef04b71fc80b32ddde740f3b9e57
Sefinek <sefinek5916@gmail.com>
2022-06-25 06:47:33 +0200

    v1.8.2

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	port: '4180',
+	port: 4180,
 };
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,9 @@ module.exports = {
 	apps: [{
 		name            : 'temporary-website',
 		script          : './index.js',
+
 		log_date_format : 'HH:mm:ss, DD.MM.YYYY',
 		error_file      : '/home/ubuntu/logs/www/temporary-website/error.log',
 		out_file        : '/home/ubuntu/logs/www/temporary-website/out.log',
 	}],
-
-	deploy: { production: { user: 'node' } },
 };
